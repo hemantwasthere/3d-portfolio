b124832b019799c280c134aaed3406e5b7fd40af
Hemant <hrsmart2150@gmail.com>
2023-04-27 11:25:02 +0530

    Removed unused files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,44 +1,36 @@
 import alive from "./alive.ico";
 import backend from "./backend.png";
-import carrent from "./carrent.png";
+import chatgpt from "./chatgpt.png";
 import close from "./close.svg";
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import cloudimg from "./cloudimg.png";
 import creator from "./creator.png";
 import demo_icon from "./demo_icon.png";
+import devsnest from "./devsnest.png";
 import digi from "./digi.svg";
+import gitalt from "./gitalt.png";
 import github from "./github.png";
-import jobit from "./jobit.png";
 import logo from "./logo.svg";
 import menu from "./menu.svg";
 import mobile from "./mobile.png";
+import sumz from './sumz.png';
+import clang from "./tech/clang.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
+import java from "./tech/java.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
+import mysql from "./tech/mysql.png";
+import nextjs from "./tech/nextjs.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
 import typescript from "./tech/typescript.png";
-import tripguide from "./tripguide.png";
-import web from "./web.png";
-import devsnest from "./devsnest.png";
-import nextjs from "./tech/nextjs.png";
 import vuejs from "./tech/vuejs.png";
-import java from "./tech/java.png";
-import clang from "./tech/clang.png";
-import mysql from "./tech/mysql.png";
-import chatgpt from "./chatgpt.png";
-import gitalt from "./gitalt.png";
-import cloudimg from "./cloudimg.png";
-import sumz from './sumz.png'
+import web from "./web.png";
 
 export {
   logo,
@@ -61,14 +53,6 @@ export {
   redux,
   tailwind,
   typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   demo_icon,
   alive,
   digi,
@@ -83,3 +67,4 @@ export {
   cloudimg,
   sumz
 };
+
